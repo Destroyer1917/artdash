db364b96bad494cc5ec0e84c0b8257cf6ce16af2
ArtemDasha <artelliseya@gmail.com>
2025-01-27 06:46:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-document.getElementById('envelope').addEventListener('click', function() {
-    // Анимация открытия конверта
-    document.querySelector('.flap.top').style.transform = 'rotateX(180deg)';
-    document.querySelector('.flap.bottom').style.transform = 'rotateX(-180deg)';
-    document.querySelector('.envelope').style.transform = 'scale(0.5)';
-    
-    // Показ приглашения с анимацией
-    setTimeout(function() {
-        document.getElementById('invitation').style.display = 'block';
-        document.getElementById('invitation').style.opacity = '1';
-        document.getElementById('invitation').style.transform = 'scale(1)';
-    }, 800); // Задержка для завершения анимации конверта
-});
 .envelope {
     width: 200px; /* Подберите размеры изображения */
     height: 150px; /* Подберите размеры изображения */
