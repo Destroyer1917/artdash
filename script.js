bf73bc55c7c5d10955fd467a2f83d38bc5345244
ArtemDasha <artelliseya@gmail.com>
2025-01-27 06:54:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,26 @@ const topFlap = document.querySelector('.top');
 const bottomFlap = document.querySelector('.bottom');
 const invitation = document.querySelector('.invitation');
 
+// Таймер
+const countdown = () => {
+    const weddingDate = new Date("July 5, 2025 00:00:00").getTime();
+    const now = new Date().getTime();
+    const timeLeft = weddingDate - now;
+
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+}
+
+// Обновляем таймер каждую секунду
+setInterval(countdown, 1000);
+
 // Функция для открытия конверта
 envelope.addEventListener('click', function() {
     // Анимация раскрытия верхней и нижней части конверта
