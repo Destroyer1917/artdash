0bcc3da65b05fd14b63eeb1e4664dc6423958712
ArtemDasha <artelliseya@gmail.com>
2025-01-27 06:43:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,14 @@ document.getElementById('envelope').addEventListener('click', function() {
         document.getElementById('invitation').style.transform = 'scale(1)';
     }, 800); // Задержка для завершения анимации конверта
 });
+.envelope {
+    width: 200px; /* Подберите размеры изображения */
+    height: 150px; /* Подберите размеры изображения */
+    background-image: url('IMG_0470.png'); /* Путь к изображению */
+    background-size: cover; /* Чтобы изображение растягивалось */
+    background-position: center; /* Центрируем изображение */
+    margin: 0 auto; /* Центрируем конверт */
+    position: relative;
+    cursor: pointer;
+    transition: transform 1s ease;
+}
